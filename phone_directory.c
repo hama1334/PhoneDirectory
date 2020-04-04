66adfcffaf201143c872bc59a0bbe096d991a633
unknown <tsingos@vtrip.net>
2020-04-05 00:43:57 +0300

    Some more true progress

--- phone_directory.c
+++ phone_directory.c
@@ -1,53 +1,147 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-typedef struct dir_entry
+#ifndef __cplusplus
+typedef char bool;
+#define TRUE 1
+#define FALSE 0
+#endif
+
+
+struct dir_entry
 {
 	char *name;
 	char *surname;
 	char *phone;
 	struct dir_entry *next;
 	struct dir_entry *prev;
-} dir_entry;
+};
+
+typedef struct dir_entry entry;
 
 typedef struct directory
 {
-	dir_entry *first;
-	dir_entry *last;
+	entry *first;
+	entry *last;
 } directory;
 
-directory *dir_init(directory *dir)
+void dir_init(directory *dir)
 {
-		directory *d;
+	dir->first = NULL; 
+	dir->last = NULL;
 		
-		d = (directory *)malloc(sizeof(directory));
-				
-		d=dir;
-		d->first = d->last = NULL;
-		
-		return d;
 }
 
-directory *insert(directory *dir, dir_entry *entry)
+bool dir_empty(directory *dir)
 {
-/*	directory *d;
-	dir_entry *e;
-	
-	if (dir == NULL)
-		d=dir_init(dir);
-	if (d->first == d->last)
+	return (NULL == dir->first)? TRUE : FALSE;
+}
+
+entry *dir_begin(directory *dir)
+{
+	return dir->first;
+}
+
+entry *dir_end(directory *dir)
+{
+	return dir->last;
+}
+
+entry *dir_next(entry *element)
+{
+	return element->next;
+}
+
+void PhoneDirectoryInsert(directory *dir, entry *element)
+{
+	directory *temp;
+	entry *temp_entry;
+		
+	if (dir_empty(dir))
 	{
-		e = (dir_entry *)malloc(sizeof(dir_entry));
-	} */
+		dir->first = element;
+		dir->last = element;
+	}
+	else
+	{
+		if (strcmp(element->surname, dir->last->surname) >= 0)
+		{
+			temp_entry = (entry *)malloc(sizeof(entry));
+			temp_entry = dir->last;
+			dir->last->next = element;
+			dir->last = element;
+			dir->last->prev = temp_entry;
+			element->next = NULL;
+		}
+		else if (strcmp(element->surname, dir->last->surname) < 0)
+		{
+			temp = (directory *)malloc(sizeof(directory));
+			temp = dir;
+			temp->last = dir->last->prev;
+			PhoneDirectoryInsert(temp, element);
+		}
+	}
+	
+}	
+
+entry *PhoneDirectoryFind(directory *dir, entry *element)
+{
+	directory *temp;
+	
+	if (strcmp(dir->first->surname, element->surname) == 0)
+		return dir->first;
+	else 
+	{
+		if (dir->first->next != NULL)
+		{
+			temp = (directory *)malloc(sizeof(directory));
+			temp = dir->first->next;
+			return PhoneDirectoryFind(temp, element);
+		}
+		else
+			return NULL;
+	}
+	
 }
-
-dir_entry *find(directory *dir, char *argum);
-
-directory *delete(directory *dir, char *argum) ;
 
 int main(void)
 {
-	directory *dir;
+	directory *phonebook = (directory *)malloc(sizeof(directory));
+	
+	entry *a = (entry *)malloc(sizeof(entry));
+    	entry *b = (entry *)malloc(sizeof(entry));
+	entry *c = (entry *)malloc(sizeof(entry));
+
+	a->name = "Katerina";
+	a->surname = "Kastriti";
+	a->phone = "6972781433";
+
+	b->name = "Dimitris";
+	b->surname = "Tsingos";
+	b->phone = "6944563690";
+
+	c->name = "Starttech";
+	c->surname = "Ventures";
+	c->phone = "2118001709";
+
+
+	dir_init(phonebook);
+	PhoneDirectoryInsert(phonebook, a);
+	PhoneDirectoryInsert(phonebook, b);
+	PhoneDirectoryInsert(phonebook, c);
+	
+	for (a = (entry *)dir_begin(phonebook);a ;a=(entry *)dir_next(a))
+	{
+		printf("Name: %-15s Surname: %-15s Phone number: %-15s\n", a->name, a->surname, a->phone);
+	}
+
+	while (!dir_empty(phonebook))
+	{
+		a = phonebook->first;
+		phonebook->first = phonebook->first->next;
+		free(a);
+	}
 
 	return 0;
 }	
