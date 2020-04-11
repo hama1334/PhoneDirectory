1356a5e2e05be1c7af530855947f7e938bfb39c9
unknown <tsingos@vtrip.net>
2020-04-11 23:44:10 +0300

    It finally works relatively well!

--- phone_directory.c
+++ phone_directory.c
@@ -8,6 +8,7 @@ typedef char bool;
 #define FALSE 0
 #endif
 
+#define MAXLEN 25
 
 struct PhoneDirectoryEntry
 {
@@ -26,199 +27,311 @@ typedef struct PhoneDirectory
 	entry *last;
 } directory;
 
-void PhoneDirectoryInitialize(directory *dir)
+entry *EntryInitialize(char *n, char *s, char *p, int len)
 {
-	dir->first = NULL; 
-	dir->last = NULL;
-	
-	return;
+	entry *temp;
+	temp = (entry *)malloc(sizeof(entry));
+	temp->name = (char *)malloc(len*sizeof(char));
+	temp->surname = (char *)malloc(len*sizeof(char));
+	temp->phone = (char *)malloc(len*sizeof(char));
+	temp->name=strdup(n);
+	temp->surname=strdup(s);
+	temp->phone=strdup(p);
+	temp->next = NULL;
+	temp->prev = NULL;
+	fprintf(stdout,"\nA new record has been created.\n");
+	return temp;
 }
 
-bool PhoneDirectoryIsEmpty(directory *dir)
+directory * DirectoryInitialize(directory *dir, entry *element)
+{
+	dir = (directory *)malloc(sizeof(directory));
+	dir->first = element;
+	dir->last = element;
+	element->next = NULL;
+	element->prev = NULL;
+	fprintf(stdout, "\nThe PhoneDirectory has been initialized and the first entry has been recorded.\n");
+	return dir;
+}
+
+bool DirectoryIsEmpty(directory *dir)
 {
 	return (NULL == dir->first)? TRUE : FALSE;
 }
 
-entry *PhoneDirectoryBegin(directory *dir)
+entry *DirectoryBegin(directory *dir)
 {
 	return dir->first;
 }
 
-entry *dir_end(directory *dir)
+entry *DirectoryEnd(directory *dir)
 {
 	return dir->last;
 }
 
-entry *dir_next(entry *element)
+entry *NextElement(entry *element)
 {
 	return element->next;
 }
 
-
-
-void PhoneDirectoryInsert(directory *dir, entry *element)
+entry *PrevElement(entry *element)
 {
-	directory *temp;
-	entry *temp_entry;
-		
-	if (PhoneDirectoryIsEmpty(dir))
+	return element->prev;
+}
+
+directory *DirectoryInsert(directory *dir, entry *element)
+{
+	entry *p, *q;
+
+	if (DirectoryIsEmpty(dir))
 	{
+	    fprintf(stdout,"\nstarting the add process\n");
 		dir->first = element;
 		dir->last = element;
+		element->prev = NULL;
+		element->next = NULL;
+		fprintf(stdout,"\nThe entry has been registered. Thank you for using the PhoneDirectory application!\n");
+		return dir;
 	}
 	else
 	{
-		if (strcmp(element->surname, dir->last->surname) >= 0)
-		{
-			temp_entry = (entry *)malloc(sizeof(entry));
-			temp_entry = dir->last;
-			dir->last->next = element;
-			dir->last = element;
-			dir->last->prev = temp_entry;
-			element->next = NULL;
-		}
-		else if (strcmp(element->surname, dir->last->surname) < 0)
-		{
-			temp = (directory *)malloc(sizeof(directory));
-			temp = dir;
-			temp->last = dir->last->prev;
-			PhoneDirectoryInsert(temp, element);
-		}
-	}
-	
-}	
+		for(p=DirectoryBegin(dir); p->next != NULL; p=NextElement(p))
+            ;
+        q=p;
+        p->next=element;
+        element->next = NULL;
+        element->prev = q;
+        dir->last = element;
+        fprintf(stdout,"\nA new entry has been registered.\n");
+        return dir;
+    }
 
-entry *PhoneDirectoryFind(directory *dir, entry *element)
-{
-	directory *temp;
-	temp = (directory *)malloc(sizeof(directory));
-	
-	if (strcmp(dir->first->surname, element->surname) == 0)
-		return dir->first;
-	else 
-	{
-		if (dir->first->next != NULL)
-		{
-			temp = dir->first->next;
-			return PhoneDirectoryFind(temp, element);
-		}
-		else
-			return NULL;
-	}
-	
 }
 
-void PhoneDirectoryPrint(directory *dir)
+entry *DirectoryFind(directory *dir, char *s)
 {
-	entry *record;
-	int i = 0;
-	record = (entry *)malloc(sizeof(entry));
-	
-	record = PhoneDirectoryBegin(dir);
-	
+	entry *p;
+
+	if (DirectoryIsEmpty(dir))
+	{
+		fprintf(stderr, "\n***error***: The PhoneDirectory is empty.\n");
+		return NULL;
+	}
+	else
+		for(p=DirectoryBegin(dir); p != NULL; p=NextElement(p))
+			if (strcmp(p->surname, s) == 0)
+				return p;
+		return NULL;
+}
+
+directory *EntryDelete(directory *dir, char *s)
+{
+
+	entry *p, *q;
+
+	p = DirectoryFind(dir, s);
+
+	if (p == NULL)
+	{
+		fprintf(stderr, "\n***error***Cannot delete a non-existing record\n");
+		return dir;
+	}
+	else if (p == DirectoryBegin(dir) && p == DirectoryEnd(dir))
+	{
+		dir->first= NULL;
+		dir->last= NULL;
+		return dir;
+	}
+//	else if (p->next != NULL && p->prev == NULL)
+	else if (p == DirectoryBegin(dir))
+	{
+		q = p->next;
+		dir->first = q;
+		q->prev = NULL;
+		return dir;
+//free(p);
+	}
+//	else if (p->prev != NULL && p->next == NULL)
+	else if (p == DirectoryEnd(dir))
+	{
+		fprintf(stdout, "\nI have found the last element\n");
+		q = p;
+		p = p->prev;
+		dir->last = p;
+		p->next = NULL;
+		return dir;
+		//free(q);
+	}
+	else
+	{
+		q = p->prev;
+		q->next = p->next;
+		p->next->prev = q;
+		//free(p);
+		return dir;
+	}
+
+}
+
+void DirectoryPrint(directory *dir)
+{
+	entry *p;
+	int i=1;
+
+	p = DirectoryBegin(dir);
+	do
+		fprintf(stdout, "No:%3d,Name:%10s Surname: %15s Phone:%13s\n", i++, p->name, p->surname, p->phone);
+	while ((p=NextElement(p))!= NULL);
+	return;
+}
+
+void RequestNewDataEntry(void)
+{
+	fprintf(stdout,"\n\nThe choices available are the following:\n");
+	fprintf(stdout,"A - Add a record, S - Search for a record, D - Delete a record, P - Print the PhoneDirectory, and, E - Exit the PhoneDirectory Application\n");
+	fprintf(stdout,"Please give your choice: ");
+
+	return;
+
+}
+
+entry *GetNewEntry(int l)
+{
+	entry *temp;
+	char *n, *s, *p;
+
+//	temp = (entry *)malloc(sizeof(entry);
+	n = (char *)malloc(l*sizeof(char));
+	fprintf(stdout,"Name: ");
+	fscanf(stdin, "%s", n);
+
+	s = (char *)malloc(l*sizeof(char));
+	fprintf(stdout,"Surname: ");
+	fscanf(stdin, "%s", s);
+
+	p = (char *)malloc(l*sizeof(char));
+	fprintf(stdout,"Phone number (international format): ");
+	fscanf(stdin,"%s", p);
+
+	temp = EntryInitialize(n, s, p, l);
+
+	return temp;
+}
+
+void PrintEntry(entry *record)
+{
 	if (record == NULL)
 	{
-		printf("The PhoneDirectory is empty\n");
+		fprintf(stderr, "\n***error: Cannot print an empty entry***\n");
 		return;
 	}
-	
-	while (record!=NULL)
+	else
 	{
-		printf("No: %d, Name: %s, Surname: %s, Phone: %s\n", i+1, record->name, record->surname, record->phone);
-		++i;
-		record = record->next;
+		fprintf(stdout,"\nThe person you are looking for is the following:\nName: %15s\tSurname: %15s\tPhone number: %13s\n\nThank you for using the PhoneDirectory Application\n",record->name, record->surname, record->phone);
+		return;
 	}
-	
+}
+
+void InitialMessageToUser()
+{
+	fprintf(stdout, "Welcome to The PhoneDirectory Application\n");
+	fprintf(stdout, "(c) Dimitris Tsingos 2020, email: tsingos@vtrip.net\n\n");
+
+	fprintf(stdout, "The choices available are the following:\n");
+	fprintf(stdout, "A - Add a record\n");
+	fprintf(stdout, "S - Search for a record\n");
+	fprintf(stdout, "D - Delete a record\n");
+	fprintf(stdout, "P - Print the PhoneDirectory\n\n");
+	fprintf(stdout, "E - Exit the PhoneDirectory Application\n\n");
+	fprintf(stdout, "Please give your choice: ");
+
 	return;
+}
+
+void FreePhoneDirecory(directory *dir)
+{
+	entry *temp, *q;
+
+	for(temp = DirectoryBegin(dir);(temp=NextElement(temp))!=NULL;)
+	{
+		q = temp;
+		free(q);
+	}
+	free(dir);
 }
 
 int main(void)
 {
-	directory *phonebook = (directory *)malloc(sizeof(directory));
-	entry *record = (entry *)malloc(sizeof(entry));
-
-	printf("Welcome to The PhoneDirectory Application\n");
-	printf("(c) Dimitris Tsingos 2020, email: tsingos@vtrip.net\n\n");
-	
-	PhoneDirectoryInitialize(phonebook);
-
-	printf("The choices available are the following:\n");
-	printf("A - Add a record\n");
-	printf("B - Search for a record\n\n");
-	printf("P - Print the PhoneDirectory\n\n");
-	printf("E - Exit the PhoneDirectory Application\n\n");
-	printf("Please give your choice: ");
-	
+	directory *PhoneBook;
+	entry *record;
+	char *s;
+	int len = MAXLEN;
 	int c;
+
+	InitialMessageToUser();
 
 	while ((c=getchar()) != EOF)
 	{
 		if (c== 'E' || c == 'e')
+		{
+			fprintf(stdout, "\nThank you for using the PhoneDirectory Application.\n");
 			break;
+		}
 		else if (c == 'A' || c == 'a')
 		{
-			record->name = (char *)malloc(20*sizeof(char));
-			printf("Name: ");
-			scanf("%s", record->name);
-
-			record->surname = (char *)malloc(20*sizeof(char));
-			printf("Surname: ");
-			scanf("%s", record->surname);
-			
-			record->phone = (char *)malloc(20*sizeof(char));
-			printf("Phone number (international format): ");
-			scanf("%s", record->phone);
-			
-			PhoneDirectoryInsert(phonebook, record);
-			
-			printf("\nThe entry has been registered. Thank you for using the PhoneDirectory application!\n");
-			printf("\n\nThe choices available are the following:\n");
-			printf("A - Add a record, B - Search for a record, P - Print the PhoneDirectory, and, E - Exit the PhoneDirectory Application\n");
-			printf("Please give your choice: ");
-			
+			if (DirectoryIsEmpty(PhoneBook))
+			{
+				record=GetNewEntry(len);
+				PhoneBook = DirectoryInitialize(PhoneBook, record);
+			}
+			else
+			{
+				record = GetNewEntry(len);
+				PhoneBook = DirectoryInsert(PhoneBook, record);
+			}
+			RequestNewDataEntry();
 			continue;
 		}
-		else if (c == 'B' || c == 'b')
+		else if (c == 'S' || c == 's')
 		{
-			entry *tempo;
-			tempo = (entry *)malloc(sizeof(entry));
 			printf("Please give the surname of the person you wish to find: ");
-			scanf("%s", record->surname);
-			tempo = PhoneDirectoryFind(phonebook, record);
-			printf("\nThe person you are looking for is the following:\nName: %s\tSurname: %s\tPhone number: %s\n\nThank you for using the PhoneDirectory Application\n",tempo->name, tempo->surname, tempo->phone);
-			free(tempo);
-			printf("\n\nThe choices available are the following:\n");
-			printf("A - Add a record, B - Search for a record, P - Print the PhoneDirectory, and, E - Exit the PhoneDirectory Application\n");
-			printf("Please give your choice: ");
+			s = (char *)malloc(len*sizeof(char));
+			fscanf(stdin, "%s", s);
+			PrintEntry(record = DirectoryFind(PhoneBook, s));
+			RequestNewDataEntry();
 			continue;
-
+		}
+		else if (c == 'D' || c == 'd')
+		{
+			printf("Please give the surname of the person's entry you wish to delete: ");
+			s = (char *)malloc(len*sizeof(char));
+			fscanf(stdin, "%s", s);
+			if ((PhoneBook = EntryDelete(PhoneBook, s)) == NULL)
+				fprintf(stderr, "\n*** The PhoneDirectory is now empty***\n");
+			RequestNewDataEntry();
+			continue;
 		}
 		else if (c == 'P' || c == 'p')
 		{
-			PhoneDirectoryPrint(phonebook);
-			printf("\n\nThe choices available are the following:\n");
-			printf("A - Add a record, B - Search for a record, P - Print the PhoneDirectory, and, E - Exit the PhoneDirectory Application\n");
-			printf("Please give your choice: ");
+			if (DirectoryIsEmpty(PhoneBook))
+				fprintf(stderr, "\nThe PhoneDirectory is empty.\n");
+			else
+				DirectoryPrint(PhoneBook);
+			RequestNewDataEntry();
 			continue;
 		}
 		else if (c == '\n' || c == ' ' || c == '\t')
 			continue;
 		else
 		{
-			printf("\nYou have not made a valid choice. Kindly select A or B or E\n");
-			continue;
+		    fprintf(stdout, "You have made a wrong choice");
+		    RequestNewDataEntry();
+		    continue;
 		}
 	}
 
-	while (!PhoneDirectoryIsEmpty(phonebook))
-	{
-		record = phonebook->first;
-		phonebook->first = phonebook->first->next;
-		free(record);
-	}
-	
-	free(phonebook);
+	FreePhoneDirecory(PhoneBook);
 
 	return 0;
-	//this is a draft comment
-}	
+
+}
