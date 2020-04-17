00d57cc1c10cbe463d5be78d4f9f872643e1c1d6
unknown <tsingos@vtrip.net>
2020-04-17 22:54:59 +0300

    A first version with separate source code files and also a header file

---
+++ PhoneDirectory_v0.2/PhoneDirectory_Data_v0.2.c
@@ -0,0 +1,1 @@
+#include "PhoneDirectory_v0.2.h"
--- PhoneDirectory_v0.2/PhoneDirectory_v0.2.c
+++ PhoneDirectory_v0.2/PhoneDirectory_v0.2.c
@@ -1,32 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#ifndef __cplusplus
-typedef char bool;
-#define TRUE 1
-#define FALSE 0
-#endif
-
-#define MAXLEN 25
-
-struct PhoneDirectoryEntry
-{
-	char *name;
-	char *surname;
-	char *phone;
-	struct PhoneDirectoryEntry *next;
-	struct PhoneDirectoryEntry *prev;
-};
-
-typedef struct PhoneDirectoryEntry entry;
-
-typedef struct PhoneDirectory
-{
-	entry *first;
-	entry *last;
-	int count;
-} directory;
+#include "PhoneDirectory_v0.2.h"
 
 entry *EntryInitialize(char *n, char *s, char *p, int len)
 {
@@ -40,7 +12,7 @@ entry *EntryInitialize(char *n, char *s, char *p, int len)
 	temp->phone=strdup(p);
 	temp->next = NULL;
 	temp->prev = NULL;
-//	fprintf(stderr,"\nA new record has been created.\n");
+
 	return temp;
 }
 
@@ -54,7 +26,7 @@ directory * DirectoryInitialize(directory *dir, entry *element)
 	dir->count = 1;
 	element->next = NULL;
 	element->prev = NULL;
-//	fprintf(stderr, "\nThe PhoneDirectory has been initialized and the first entry has been recorded.\n");
+
 	return dir;
 }
 
@@ -108,7 +80,6 @@ bool EntriesAreIdentical(entry *a, entry *b)
 
 directory *DirectoryInsert(directory *dir, entry *element)
 {
-//	fprintf(stderr, "\n***The DirectoryInsert function starts here***\n");
 	entry *p, *pprev;
 	
 		for(p=DirectoryBegin(dir); p != NULL && (EntryCmp(p, element) < 0); pprev = p, p=NextElement(p))
@@ -135,7 +106,7 @@ directory *DirectoryInsert(directory *dir, entry *element)
 			p->prev=element;
 		}
 		++(dir->count);
-//		fprintf(stderr,"\nA new entry has been registered.\n");
+
 		return dir;
 }
 
@@ -181,106 +152,25 @@ directory *EntryDelete(directory *dir, char *s)
 		q->prev = NULL;
 		--(dir->count);
 		return dir;
-// free(p);
 	}
 	else if (p == DirectoryEnd(dir))
 	{
-// fprintf(stdout, "\nI have found the last element\n");
 		q = p;
 		p = p->prev;
 		dir->last = p;
 		p->next = NULL;
 		--(dir->count);
 		return dir;
-// free(q);
 	}
 	else
 	{
 		q = p->prev;
 		q->next = p->next;
 		p->next->prev = q;
-		//free(p);
 		--(dir->count);
 		return dir;
 	}
 
-}
-
-void DirectoryPrint(directory *dir)
-{
-	entry *p;
-	int i=1;
-
-	p = DirectoryBegin(dir);
-	do
-		fprintf(stdout, "No:%3d,Name:%10s Surname: %15s Phone:%13s\n", i++, p->name, p->surname, p->phone);
-	while ((p=NextElement(p))!= NULL);
-//	while ((p=NextElement(p))!= NULL && --(dir->count));
-	return;
-}
-
-void RequestNewDataEntry(void)
-{
-	fprintf(stdout,"\n\nThe choices available are the following:\n");
-	fprintf(stdout,"A - Add a record, S - Search for a record, D - Delete a record, V - Print the number of the PhoneDirectory records, P - Print the PhoneDirectory, and, E - Exit the PhoneDirectory Application\n");
-	fprintf(stdout,"Please give your choice: ");
-
-	return;
-
-}
-
-entry *GetNewEntry(int l)
-{
-	entry *temp;
-	char *n, *s, *p;
-
-//	temp = (entry *)malloc(sizeof(entry);
-	n = (char *)malloc(l*sizeof(char));
-	fprintf(stdout,"Name: ");
-	fscanf(stdin, "%s", n);
-
-	s = (char *)malloc(l*sizeof(char));
-	fprintf(stdout,"Surname: ");
-	fscanf(stdin, "%s", s);
-
-	p = (char *)malloc(l*sizeof(char));
-	fprintf(stdout,"Phone number (international format): ");
-	fscanf(stdin,"%s", p);
-
-	temp = EntryInitialize(n, s, p, l);
-
-	return temp;
-}
-
-void PrintEntry(entry *record)
-{
-	if (record == NULL)
-	{
-		fprintf(stderr, "\n***error: Cannot print an empty entry***\n");
-		return;
-	}
-	else
-	{
-		fprintf(stdout,"\nThe person you are looking for is the following:\nName: %15s\tSurname: %15s\tPhone number: %13s\n\nThank you for using the PhoneDirectory Application\n",record->name, record->surname, record->phone);
-		return;
-	}
-}
-
-void InitialMessageToUser()
-{
-	fprintf(stdout, "Welcome to The PhoneDirectory Application\n");
-	fprintf(stdout, "(c) Dimitris Tsingos 2020, email: tsingos@vtrip.net\n\n");
-
-	fprintf(stdout, "The choices available are the following:\n");
-	fprintf(stdout, "A - Add a record\n");
-	fprintf(stdout, "S - Search for a record\n");
-	fprintf(stdout, "D - Delete a record\n");
-	fprintf(stdout, "V - Print the number of the PhoneDirectory records\n");
-	fprintf(stdout, "P - Print the PhoneDirectory\n\n");
-	fprintf(stdout, "E - Exit the PhoneDirectory Application\n\n");
-	fprintf(stdout, "Please give your choice: ");
-
-	return;
 }
 
 void FreePhoneDirecory(directory *dir)
